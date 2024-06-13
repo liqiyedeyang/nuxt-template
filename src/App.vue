ee768dd614a7cc7ce0ad98cfe1ad0369ae21831e
李七夜的羊 <lxy81122022@163.com>
2024-06-13 21:01:21 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup>
+import "@unocss/reset/tailwind-compat.css"; //重置html标签默认样式
+</script>
+
+<style>
+button,
+[type="button"],
+[type="reset"],
+[type="submit"] {
+  -webkit-appearance: button; /* 1 */
+  background-color: transparent; /* 2 */
+  background-image: none; /* 2 */
+}
+</style>
